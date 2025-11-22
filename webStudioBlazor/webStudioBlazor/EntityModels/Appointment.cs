899299d80578f223067c32afe573d23ef1c0c6a9
Vasyl Stolyarchuk <Vasyl.Stolyarchuk@outlook.com>
2025-11-22 17:56:32 +0200

    added time for vanicure master and update validate

--- webStudioBlazor/webStudioBlazor/EntityModels/Appointment.cs
+++ webStudioBlazor/webStudioBlazor/EntityModels/Appointment.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
+using System.ComponentModel.DataAnnotations;
+
 using webStudioBlazor.Data;
 
 namespace webStudioBlazor.EntityModels
@@ -7,21 +9,44 @@ namespace webStudioBlazor.EntityModels
     public class Appointment
     {
         public int Id { get; set; }
-        public string ClientName { get; set; }
-        public string ClientPhone { get; set; }
+
+        [Required(ErrorMessage = "Введіть ім’я та прізвище")]
+        [StringLength(100, ErrorMessage = "Ім'я не повинно перевищувати 100 символів")]
+        [MinLength(3, ErrorMessage = "Ім'я занадто коротке")]
+        public string ClientName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Введіть номер телефону")]
+        [RegularExpression(@"^(\+380|0)\d{9}$", ErrorMessage = "Формат телефону: +380XXXXXXXXX")]
+        public string ClientPhone { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Оберіть дату запису")]
         public DateOnly AppointmentDate { get; set; }
+
+        [Required(ErrorMessage = "Оберіть час запису")]
         public TimeOnly SetHour { get; set; }
+               
         public int MasterId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Оберіть категорію")]
         public int CategoryId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Оберіть послугу")]
         public int TherapyId { get; set; }
+             
         public decimal Price { get; set; }
-        public bool IsCompleted { get; set; }
-        public string? UserId { get; set; } = default!;
-        public ApplicationUser? User { get; set; } = default!;
-        public Master Master { get; set; } = default!;
-        public Category Category { get; set; } = default!;
-        public TherapyCard TherapyCard { get; set; } = default!;
+
+        public bool IsCompleted { get; set; } = false;
+
+        public string? UserId { get; set; }
+
+        public ApplicationUser? User { get; set; }
+
+        public Master? Master { get; set; }
+
+        public Category? Category { get; set; }
+
+        public TherapyCard? TherapyCard { get; set; }
+
         public List<AppointmentService?> AppointmentServices { get; set; } = new();
-       
     }
 }
